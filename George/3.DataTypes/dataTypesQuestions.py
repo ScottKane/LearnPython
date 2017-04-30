69f259d34aa3eff541d752dfbf1772b8a61f9855
George Wells <George Wells>
2017-04-30 18:34:35 +0100

    added Datatypes material

---
+++ George/3.DataTypes/dataTypesQuestions.py
@@ -0,0 +1,26 @@
+#What would you expect to be output by running the following examples?
+
+#1
+x = 1
+y = 2
+z = x + y
+print (z)
+
+
+#2
+x = "1"
+y = "2"
+z = x + y
+print (z)
+
+#3
+x = "1"
+y = 2
+z = x + y
+print (z)
+
+#4
+x = 1.5
+y = 2
+z = x + y
+print (z)
